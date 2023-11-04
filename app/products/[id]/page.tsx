2308304abbe08f70b05756649d8a1327138dc2cc
Mahone <87675992+ID-Mahone@users.noreply.github.com>
2023-11-04 02:04:14 +0100

    update

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,9 +1,12 @@
-import { getProductById } from "@/lib/actions"
+import { getProductById, getSimilarProducts } from "@/lib/actions"
 import { redirect } from "next/navigation"
 import Image from "next/image"
 import Link from 'next/link';
 import { formatNumber } from "@/lib/utils";
 import PriceInfoCard from "@/components/PriceInfoCard";
+import { Product } from "@/types";
+import ProductCard from "@/components/ProductCard";
+
 
 type Props = {
     params: { id: string }
@@ -13,6 +16,8 @@ const ProductDetails = async ({ params: { id } }: Props) => {
     const product = await getProductById(id);
 
     if (!product) redirect('/')
+
+    const similarProducts = await getSimilarProducts(id);
 
     return (
         <div className="product-container">
@@ -138,7 +143,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
                 </div>
             </div>
 
-            <div className="flex flex-col gap-16 border-2 border-red-500">
+            <div className="flex flex-col gap-16 border-2">
                 <div className="flex flex-col gap-5">
                     <h3 className="text-2xl text-secondary font-semibold">
                         Product Description
@@ -162,6 +167,19 @@ const ProductDetails = async ({ params: { id } }: Props) => {
                     </Link>
                 </button>
             </div>
+
+            {similarProducts && similarProducts.length > 0 && (
+                <div className="py-14 flex flex-col gap-2 w-full">
+                    <p className="section-text">Similar Products</p>
+
+                    <div className="flex flex-wrap gap-10 mt-7 w-full">
+                        {similarProducts.map((product) => (
+                            <ProductCard key={product._id} product={product} />
+                        ))}
+                    </div>
+
+                </div>
+            )}
         </div>
     )
 }
